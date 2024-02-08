71ad460764508711898d6a66d29d97b80759e775
bharath17reddy <118371898+bharath17reddy@users.noreply.github.com>
2024-02-08 14:52:56 +0530

    Update main.tf

--- Question3/main.tf
+++ Question3/main.tf
@@ -6,10 +6,10 @@ resource "aws_key_pair" "example" {
   public_key = file("~/.ssh/id_rsa.pub")   
 }
 resource "aws_instance" "example" {
-  ami           = "ami-0c7217cdde317cfec"  # Linux AMI ID, you may need to change this based on your region
+  ami           = "ami-0c7217cdde317cfec"   
   instance_type = var.InstanceType
   subnet_id     = var.SubnetId
-  key_name      = var.KeyName
+  key_name      = aws_key_pair.example.key_name
   vpc_id        = var.vpcId
 
   tags = {
