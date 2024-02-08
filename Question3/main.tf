fd306634b339bec51eba6bbb1b7486f5b2237e58
bharath17reddy <118371898+bharath17reddy@users.noreply.github.com>
2024-02-08 14:50:34 +0530

    Update main.tf

--- Question3/main.tf
+++ Question3/main.tf
@@ -1,7 +1,10 @@
 provider "aws" {
   region = "us-east-1"
 }
-
+resource "aws_key_pair" "example" {
+  key_name   = "bharath-key-name"  
+  public_key = file("~/.ssh/id_rsa.pub")   
+}
 resource "aws_instance" "example" {
   ami           = "ami-0c7217cdde317cfec"  # Linux AMI ID, you may need to change this based on your region
   instance_type = var.InstanceType
