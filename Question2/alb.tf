d8c041ae33feafd2d9d761c6f935006597f62347
bharath17reddy <118371898+bharath17reddy@users.noreply.github.com>
2024-02-08 17:12:30 +0530

    Update alb.tf

--- Question2/alb.tf
+++ Question2/alb.tf
@@ -1,6 +1,10 @@
  provider "aws" {
   region = "us-east-1"
 }
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = "vpc-02af0191100015353"
 
 resource "aws_lb" "example" {
   name               = "example-alb"
@@ -33,11 +37,6 @@ resource "aws_lb_target_group_attachment" "example" {
   target_group_arn = aws_lb_target_group.example.arn
   target_id        = "i-0a4d2d81da8d9f684"
 }
-
-resource "aws_security_group" "alb_sg" {
-  name        = "alb-sg"
-  description = "Security group for ALB"
-  vpc_id      ="vpc-02af0191100015353"
   
   ingress {
     description = "HTTP from VPC"
