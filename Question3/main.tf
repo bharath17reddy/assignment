1ef281d566ee2c924cabad78dff65e08fcf468f5
bharath17reddy <118371898+bharath17reddy@users.noreply.github.com>
2024-02-08 14:57:41 +0530

    Update main.tf

--- Question3/main.tf
+++ Question3/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_key_pair" "example" {
-  key_name   = "bharath-key-name"  
+  key_name   = "terraform-key-name"  
   public_key = file("~/.ssh/id_rsa.pub")   
 }
 resource "aws_instance" "example" {
@@ -16,5 +16,36 @@ resource "aws_instance" "example" {
     Name = "Example Instance"
   }
 }
+resource "aws_security_group" "webSg" {
+  name   = "web"
+  vpc_id = var.vpcId
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web-sg"
+  }
+}
+
 
 
